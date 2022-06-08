58cce2327f6d2d60e67eed12256168badf228ba5
Shiloli-r <ronaldshiloli@gmail.com>
2022-06-08 10:27:06 +0300

    get user models

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -2,14 +2,22 @@ import 'dart:typed_data';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:instagram_clone/models/user.dart' as model;
 import 'package:instagram_clone/resources/storage_methods.dart';
-import 'package:instagram_clone/utils/utils.dart';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // get user details
+  Future<model.User> getUserDetails() async {
+    User currentUser = _auth.currentUser!;
+
+    DocumentSnapshot snap =
+        await _firestore.collection('users').doc(currentUser.uid).get();
+
+    return model.User.fromSnap(snap);
+  }
 
   // Sign up user
   Future<String> signUpUser({
