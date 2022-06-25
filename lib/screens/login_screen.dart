5b24a2b0b8e571c615dc5ae2e8b8f38217de43d9
Shiloli-r <ronaldshiloli@gmail.com>
2022-06-25 15:50:20 +0300

    unused imports

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,7 +6,6 @@ import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
 import 'package:instagram_clone/screens/signup_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
-import 'package:instagram_clone/utils/utils.dart';
 import 'package:instagram_clone/widgets/text_field_input.dart';
 
 class LoginScreen extends StatefulWidget {
